ea7080060eafbe4c484162d503daa455be0b6fba
Samuel Wiseman <>
2025-11-24 11:09:40 -0600

    remove initial debug secure endpoint

--- webapp/routes/securePing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -X GET -i http://localhost:8081/api/secure/ping \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjM2NzI3MDgsInVzZXJfaWQiOjF9.M_1hmc8TmXugfizRsOU2luWdMF1byQor3GLaXVmZaxQ"
