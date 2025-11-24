d50f3efa293eea031645a128ef292da737074f51
Samuel Wiseman <>
2025-11-24 11:09:28 -0600

    add helper scripts to test functionality of user family endpoints

---
+++ webapp/routes/leaveFamily.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -X POST -i http://localhost:8081/api/secure/leave-family \
+  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjQwMDU2MTIsInVzZXJfaWQiOjF9.R0Z7z3ycOCCAef7zMK_6pfSTBHts3vgiVoDdChZwhAs"
